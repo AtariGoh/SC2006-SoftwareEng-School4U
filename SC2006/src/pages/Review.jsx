d62f538bf5d4e75d9ddf0be9065a9f4f32387b14
Armaan-king <ARMAAN002@e.ntu.edu.sg>
2024-10-14 06:02:59 +0800

    Add files via upload

---
+++ SC2006/src/pages/Review.jsx
@@ -0,0 +1,10 @@
+const Review = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Review Page</h1>
+      <p>Leave your thoughts and feedback about the schools here.</p>
+    </div>
+  );
+};
+
+export default Review;
